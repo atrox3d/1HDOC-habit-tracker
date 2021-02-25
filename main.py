6e334c3a0a8288686bb895b51aa4d3156298b056
atrox3d <atrox3d@gmail.com>
2021-02-25 19:41:39 +0100

    create graph

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import myob
-
+import json
 #
 # create user: one shot
 #
@@ -11,6 +11,28 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
+print(f"calling {PIXELA_ENDPOINT} with params: {user_params}")
 response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 print(response.text)
+#
+#   create graph: one shot
+#
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "sora"
+}
+#
+# authentication via headers, not in the url
+#
+headers = {
+    "X-USER-TOKEN": myob.PIXELA_TOKEN
+}
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{myob.PIXELA_USERNAME}/graphs"
+print(f"calling {GRAPH_ENDPOINT} with params: {graph_config} and headers: {headers}")
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+print(response.text)
+print(f"get the graph here: {GRAPH_ENDPOINT}/{graph_config['id']}.html")
 
