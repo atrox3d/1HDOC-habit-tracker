6df7eb21c98f54d6470d14858bd71913fb6b9297
atrox3d <atrox3d@gmail.com>
2021-02-26 05:51:06 +0100

    refactor with common api function

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import json
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 
+def pixela_api(endpoint, params=None, headers=None):
+    print(f"calling {endpoint}")
+    if params:
+        print(f"with params: {params}")
+    if headers:
+        print(f"with headers: {headers}")
+    response = requests.post(url=endpoint, json=params, headers=headers)
+    print("response: ", response.text)
+    return response
+
+
 def create_user(username: str, token: str):
     """
     create user: one shot
@@ -15,15 +26,16 @@ def create_user(username: str, token: str):
         "agreeTermsOfService": "yes",
         "notMinor": "yes",
     }
-    print(f"calling {PIXELA_ENDPOINT}\nwith params: {user_params}")
-    response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-    print("response: ", response.text)
+    # print(f"calling {PIXELA_ENDPOINT}\nwith params: {user_params}")
+    # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+    # print("response: ", response.text)
+    return pixela_api(PIXELA_ENDPOINT, user_params)
 
 
 #
 #   create graph: one shot
 #
-def create_graph(graph_id, name, unit, graph_type, color):
+def create_graph(username, token, graph_id, name, unit, graph_type, color):
     """
     create graph: one shot
     """
@@ -38,14 +50,16 @@ def create_graph(graph_id, name, unit, graph_type, color):
     # authentication via headers, not in the url
     #
     headers = {
-        "X-USER-TOKEN": myob.PIXELA_TOKEN
+        "X-USER-TOKEN": token
     }
-    GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{myob.PIXELA_USERNAME}/graphs"
-    print(f"calling {GRAPH_ENDPOINT}\nwith params: {graph_config}\nand headers: {headers}")
-    response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
-    print("response: ", response.text)
+    GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{username}/graphs"
+    # print(f"calling {GRAPH_ENDPOINT}\nwith params: {graph_config}\nand headers: {headers}")
+    # response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+    # print("response: ", response.text)
+    response = pixela_api(GRAPH_ENDPOINT, graph_config, headers)
     print(f"get the graph here: {GRAPH_ENDPOINT}/{graph_config['id']}.html")
+    return response
 
 
 create_user(myob.PIXELA_USERNAME, myob.PIXELA_TOKEN)
-create_graph("graph1", "Cycling Graph", "km", "float", "sora")
+create_graph(myob.PIXELA_USERNAME, myob.PIXELA_TOKEN, "graph1", "Cycling Graph", "km", "float", "sora")
