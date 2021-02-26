fbf19a2397eafa716f386e53e9f282e5ff566c09
atrox3d <atrox3d@gmail.com>
2021-02-26 05:01:49 +0100

    refactor in functions

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
 import requests
 import myob
 import json
+
+
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 #
 # create user: one shot
 #
-PIXELA_ENDPOINT = "https://pixe.la/v1/users"
-user_params = {
-    "username": myob.PIXELA_USERNAME,
-    "token": myob.PIXELA_TOKEN,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
-print(f"calling {PIXELA_ENDPOINT} with params: {user_params}")
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-print(response.text)
+def create_user(username, token):
+    user_params = {
+        "username": username,
+        "token": token,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes",
+    }
+    print(f"calling {PIXELA_ENDPOINT} with params: {user_params}")
+    response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+    print(response.text)
 #
 #   create graph: one shot
 #
-graph_config = {
-    "id": "graph1",
-    "name": "Cycling Graph",
-    "unit": "km",
-    "type": "float",
-    "color": "sora"
-}
-#
-# authentication via headers, not in the url
-#
-headers = {
-    "X-USER-TOKEN": myob.PIXELA_TOKEN
-}
-GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{myob.PIXELA_USERNAME}/graphs"
-print(f"calling {GRAPH_ENDPOINT} with params: {graph_config} and headers: {headers}")
-response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
-print(response.text)
-print(f"get the graph here: {GRAPH_ENDPOINT}/{graph_config['id']}.html")
+def create_graph(graph_id, name, unit, graph_type, color):
+    graph_config = {
+        "id":   graph_id,
+        "name": name,
+        "unit": unit,
+        "type": graph_type,
+        "color": color
+    }
+    #
+    # authentication via headers, not in the url
+    #
+    headers = {
+        "X-USER-TOKEN": myob.PIXELA_TOKEN
+    }
+    GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{myob.PIXELA_USERNAME}/graphs"
+    print(f"calling {GRAPH_ENDPOINT} with params: {graph_config} and headers: {headers}")
+    response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+    print(response.text)
+    print(f"get the graph here: {GRAPH_ENDPOINT}/{graph_config['id']}.html")
 
+
+create_user(myob.PIXELA_USERNAME, myob.PIXELA_TOKEN)
+create_graph("graph1", "Cycling Graph", "km", "float", "sora")
